912911cf0498f78113f1953fa25742c36fd89778
lapossim <21d_lapossim@nyirszikszi.hu>
2026-03-07 18:25:58 +0100

    Created BudgetTrackerController

--- budgettracker/src/main/java/hu/nyirszikszi/budgettracker/BudgetTrackerApplication.java
+++ budgettracker/src/main/java/hu/nyirszikszi/budgettracker/BudgetTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BudgettrackerApplication {
+public class BudgetTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BudgettrackerApplication.class, args);
+		SpringApplication.run(BudgetTrackerApplication.class, args);
 	}
 
 }
