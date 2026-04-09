15bc411d3d199c409e3782ce86db67eca58a1ea8
lapossim <21d_lapossim@nyirszikszi.hu>
2026-04-09 14:37:30 +0200

    Created design for cards

--- frontend/budgettracker/src/Components/Card/Card.jsx
+++ frontend/budgettracker/src/Components/Card/Card.jsx
@@ -1,8 +1,13 @@
 import styles from './Card.module.css'
 
-export default function Card({}) {
+export default function Card({title, content}) {
     return (
         <div className={styles.cardBody}>
+            <h2>{title}</h2>
+            <p>{content.p1}</p>
+            <p>{content.p2}</p>
+            <p>{content.p3}</p>
+            
         </div>
     )
 }
