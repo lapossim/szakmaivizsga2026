c31d9041530977819dc8f73f284060178ba8d445
lapossim <21d_lapossim@nyirszikszi.hu>
2026-04-15 14:43:46 +0200

    Deleted backend

--- backend/BudgetTracker/src/main/java/hu/nyirszikszi/budgettracker/service/BudgetTrackerService.java
+++
@@ -1,7 +0,0 @@
-package hu.nyirszikszi.budgettracker.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class BudgetTrackerService {
-}
--- frontend/budgettracker/src/Components/Card/Card.jsx
+++ frontend/budgettracker/src/Components/Card/Card.jsx
@@ -1,13 +1,16 @@
-import styles from './Card.module.css'
+import styles from './Card.module.css';
 
-export default function Card({title, content}) {
+export default function Card({ title, content }) {
+    const imgExists = !!content?.img;
+
     return (
         <div className={styles.cardBody}>
             <h2>{title}</h2>
+            {imgExists && <img src={content.img} alt={content.alt} />}
+            
             <p>{content.p1}</p>
             <p>{content.p2}</p>
             <p>{content.p3}</p>
-            
         </div>
-    )
+    );
 }
