73b9a0c24dcb17f010be1708201c2bd7d6ed441f
Gulyasa06 <21d_gulyasa@nyirszikszi.hu>
2026-04-26 20:12:06 +0200

    Create FinancialSummaryDto.java

---
+++ backend/src/main/java/com/example/expensetracker/dto/FinancialSummaryDto.java
@@ -0,0 +1,19 @@
+package com.example.expensetracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FinancialSummaryDto {
+
+    private BigDecimal totalIncome;
+    private BigDecimal totalExpense;
+    private BigDecimal balance;
+}
