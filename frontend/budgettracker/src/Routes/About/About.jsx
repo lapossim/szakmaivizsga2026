64ee601ad843d1471687e985a34b306ecc6ee700
lapossim <21d_lapossim@nyirszikszi.hu>
2026-04-26 18:41:50 +0200

    Removed frontend

--- frontend/budgettracker/src/Routes/About/About.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './About.module.css'
-
-export default function About() {
-    return (
-        <div className={styles.aboutBody}>
-            <h1>About</h1>
-        </div>
-    )
-}
